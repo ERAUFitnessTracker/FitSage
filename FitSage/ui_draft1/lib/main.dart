14442bff68edaa0f4928765a204951cfd0ca8500
Jack Lee <50720037+kcajeel@users.noreply.github.com>
2023-02-27 17:22:26 -0500

    git practice 2

--- FitSage/ui_draft1/lib/main.dart
+++ FitSage/ui_draft1/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'UI Draft 2.41',
+      title: 'UI Draft 2.5',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
