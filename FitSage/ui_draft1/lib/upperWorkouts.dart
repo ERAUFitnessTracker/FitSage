88389bccf9b236ec91d8ee90819b58ce3bd9d093
Danymach <machado.flia.d@gmail.com>
2023-03-27 16:33:51 -0400

    Draft of the workout menu.

---
+++ FitSage/ui_draft1/lib/upperWorkouts.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class UpperWorkouts extends StatelessWidget {
+  const UpperWorkouts({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: const [
+          Text("Helooooooooo"),
+          Text("Helooooooooo"),
+          Text("Helooooooooo"),
+          Text("Helooooooooo"),
+          Text("Helooooooooo"),
+        ],
+      ),
+    );
+  }
+}
---
+++ FitSage/ui_draft1/lib/workout.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'upperWorkouts.dart';
+//import 'package:google_fonts/google_fonts.dart';
+
+void main() => runApp(const WorkoutPage()); // Run the app
+
+class WorkoutPage extends StatelessWidget {
+  const WorkoutPage({super.key});
+
+  //This is where you create the MaterialApp. This is used to set constants for the whole app, like themes and properties
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Workout',
+      home: WorkoutHome(),
+    );
+  }
+}
+
+class WorkoutHome extends StatelessWidget {
+  const WorkoutHome({super.key});
+
+  //Making the Home page of the Workouts :3
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Scaffold(
+          body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const [
+              Upper(),
+              Lower(),
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
+
+class Lower extends StatelessWidget {
+  const Lower({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const UpperWorkouts(),
+          ),
+        );
+      },
+      child: const Card(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text("lower"),
+        ),
+      ),
+    );
+  }
+}
+
+class Upper extends StatelessWidget {
+  const Upper({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Card(
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Text("Upper"),
+      ),
+    );
+  }
+}
